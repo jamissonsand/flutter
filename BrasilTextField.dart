0c425683463e05a4fa04449444d073d7e6c9d5b6
Jamisso Andrade <111239204+jamissonsand@users.noreply.github.com>
2024-01-15 14:03:38 -0300

    Create BrasilTextField.dart

---
+++ BrasilTextField.dart
@@ -0,0 +1,73 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/custom_code/widgets/index.dart'; // Imports other custom widgets
+import '/flutter_flow/custom_functions.dart'; // Imports custom functions
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+https://github.com/jamissonsand
+// Imports custom actions
+import 'package:currency_text_input_formatter/currency_text_input_formatter.dart';
+import 'package:flutter/services.dart';
+
+class BrasilTextField extends StatefulWidget {
+  const BrasilTextField({
+    Key? key,
+    this.width,
+    this.height,
+    required this.value,
+    required this.filledColor,
+    required this.fontSize,
+    required this.colorText,
+  }) : super(key: key);
+
+  final double? width;
+  final double? height;
+  final String value;
+  final Color filledColor;
+  final double fontSize;
+  final Color colorText;
+
+  @override
+  _BrasilTextFieldState createState() => _BrasilTextFieldState();
+}
+
+class _BrasilTextFieldState extends State<BrasilTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      initialValue: widget.value,
+      keyboardType: TextInputType.number,
+      inputFormatters: [
+        CurrencyTextInputFormatter(
+          locale: 'pt_BR',
+          decimalDigits: 2,
+          symbol: 'R\$ ',
+          enableNegative: true,
+        ),
+        LengthLimitingTextInputFormatter(16),
+      ],
+      style: TextStyle(
+        fontWeight: FontWeight.normal,
+        color: widget.colorText,
+        fontSize: widget.fontSize,
+      ),
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          // ),      // Retire isso para ativar o contorno
+          borderSide: BorderSide.none,
+        ), // Isso retira o contono e fica só o preenchimento
+        filled: true,
+        fillColor: widget.filledColor,
+      ),
+      onChanged: (text) {
+        print("valor FomartValue");
+        print(text);
+        FFAppState().valueFormat =
+            text; // (IMPORTANTE) Crie um App Stat com o nome valueFormat, isso irá permitir recuperar o valor do input.
+      },
+    );
+  }
+}
